9c8b935e8e501cfc1464414989dc8b81c4e5f4f7
Tochi <31775683+Tochi-kazi@users.noreply.github.com>
2019-06-17 14:23:00 -0400

    Update MainHangMan.java

--- MainHangMan.java
+++ MainHangMan.java
@@ -6,50 +6,88 @@
 
 import java.util.*;
 
-public class MainHangMan {
+public class HangMan {
+	ArrayList<String> Words = new ArrayList<String> ();
+	ArrayList<String> Junk = new ArrayList<String> ();
+	ArrayList<String> Body = new ArrayList<String> ();
+	ArrayList<String> WordPiece = new ArrayList<String> ();
 	
-	public static void main(String[] args) {
-		HangMan letter = new HangMan ();
+	Scanner scanner = new Scanner(System.in);	 
+	
+	Random r = new Random();
+	String word = "";
+	String text = "" ;
+	
+	//generate random characters plus the real characters.
+	protected void randletter() {
+		//char c; //from 65 to 90
+		for ( char c = 'A'; c <= 'Z'; c++) {
+	        //System.out.print(c);
+	        Junk.add(Character.toString(c));
+	    }
 		
-		letter.push();
-		letter.randletter();
-		
-		String alpha = letter.text;
-		
-		System.out.println("Guess the corect " + alpha.length() + " letter word");
-		System.out.println("Use Upper Case ");
-		Game(letter);
 	}
-	public static void Game (HangMan myletter) {
-		//stop the program when all the words have been found 
-		if (myletter.WordPiece.contains("*") && myletter.Body.size() > 0) {
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("The junk "  + myletter.Junk);
-			System.out.println("The Words "  + myletter.WordPiece);
-			//check if the input is a character and if it has any of the alphabets in the ArrayList
-			String input = scanner.next();
-			if (input.matches("[A-Za-z]{1}")) {
-				//if the Junk arraylist contain the input alphabet
-				if (myletter.Junk.contains(input)) {
-					myletter.ValidLetter(input);
-					Game(myletter);
-				}else {
-					System.out.println("Invalid Choice, make sure you are using UPPER CASE and make sure the letter is an option ");
-					Game(myletter);
-					
-				}
-			}else {
-				System.out.println("Type in a valid alphabet from the list above ");
-				Game(myletter);
-			}
-		}else {
-			System.out.println("The Word is " + myletter.text);
-			if (!myletter.WordPiece.contains("*")) {
-				System.out.println("DONE, YOU WON!");
+	//put in words into the array 
+	public void push() {
+		//add words in the word bracket
+		Words.add("AWKWARD");
+		Words.add("BAGPIPES");
+		Words.add("BANJO");
+		Words.add("BUNGLER");
+		Words.add("COMPUTER");
+		Words.add("CRYPT");
+		Words.add("DWARVES");
+		Words.add("FERVID");
+		Words.add("FISHHOOK");
+		Words.add("BOOKS"); 
+		Collections.shuffle(Words);
+		//add an extericks for the blanks
+		text =  Words.get(0);
+		
+		for (int i = 0; i < text.length(); i++) {
+			WordPiece.add("*");
+		} 
+		
+		//add body Parts
+		Body.add("Head");
+		Body.add("Chest");
+		Body.add("left Hand");
+		Body.add("right Hand");
+		Body.add("left Leg");
+		Body.add("right Leg");
+		return;
+		
+	}
+
+	//check if the letter is correct and give a new list of letter if correct.
+	public void ValidLetter(String l) {
+		for (int j = 0; j < Junk.size(); j++) {
+			if (Junk.get(j).equals(l)){
+				Junk.remove(j);
+				//check if the word contains the letter.
+				if (text.contains(l)) {
+					for (int i = 0; i < text.length(); i++) {
+						//check if letter equals any letter in the input and print out the remaining
+						if (l.equals(Character.toString(text.charAt(i)))){
+							WordPiece.set(i, l);
+						}
+						
 				
-			}else {
-				System.out.println("DONE, YOU LOST! from main");
+					}
+				//else call the lose function
+				}else {
+					lose();
+				}
 			}
+		}
+	}
+	//remove a body parts after each wrong alphabet
+	public void lose () {
+		System.out.println("Wrong Guess, you lost " + Body.remove(0));
+		System.out.println("You have " + Body.size() + " more body parts remaining");
+		if (Body.size() == 0) {
+			//System.out.println("You have lost the game ");
+			scanner.close();
 		}
 	
 	}
